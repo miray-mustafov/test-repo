ebc98c7287f8967210470424981ef6ee12b371a4
miray-mustafov <mustafovmiray@gmail.com>
2024-01-15 20:26:29 +0200

    print(isprime)

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 def is_prime(a):
+    if a <= 1:
+        return False
+    elif a == 2:
+        return True
+    elif a % 2 == 0:
+        return False
     for i in range(3, int(a ** 0.5) + 1, 2):
         if a % i == 0:
             return False
     return True
 
 
-a = 5
-
-print('Hi')
-
+a = 21
 is_a_prime = is_prime(a)
-if is_a_prime:
-    print('a is prime yes')
-else:
-    print('No')
+print(is_a_prime)
