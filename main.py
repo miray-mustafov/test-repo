1d81a5a9fb4676a406df6e43b68accd2a5ddc86a
Miray Mustafov <mustafovmiray@gmail.com>
2024-01-15 20:37:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,1 @@
-def is_prime(a):
-    if a <= 1:
-        return False
-    elif a == 2:
-        return True
-    elif a % 2 == 0:
-        return False
-    for i in range(3, int(a ** 0.5) + 1, 2):
-        if a % i == 0:
-            return False
-    return True
-def abc():
-    print()
 
-a = 21
-is_a_prime = is_prime(a)
-print(is_a_prime)
