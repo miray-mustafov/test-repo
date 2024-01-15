8f6fa327d9e7f72fe11c36a5457909c62d638bc4
miray-mustafov <mustafovmiray@gmail.com>
2024-01-15 19:58:25 +0200

    las

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 def is_prime(a):
+    if a <= 1:
+        return False
+    elif a == 2:
+        return True
+    elif a % 2 == 0:
+        return False
     for i in range(3, int(a ** 0.5) + 1, 2):
         if a % i == 0:
             return False
