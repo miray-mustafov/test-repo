cbd2613b1d2c2729b54049ceca277c3417d81b0f
Miray Mustafov <mustafovmiray@gmail.com>
2024-01-15 20:07:33 +0200

    del ifs and a

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
 def is_prime(a):
-
-    elif a == 2:
-        return True
-    elif a % 2 == 0:
-        return False
     for i in range(3, int(a ** 0.5) + 1, 2):
         if a % i == 0:
             return False
@@ -11,7 +6,6 @@ def is_prime(a):
 
 
 print('Hi')
-a = 9
 
 is_a_prime = is_prime(a)
 if is_a_prime:
